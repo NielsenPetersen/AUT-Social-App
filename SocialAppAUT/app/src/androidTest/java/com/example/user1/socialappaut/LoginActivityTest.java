2e18338c2af5b6b6ba59fcd550c4a08542db0412
fwg7192 <fwg7192@autuni.ac.nz>
2017-09-25 18:48:09 +1300

    test

--- SocialAppAUT/app/src/androidTest/java/com/example/user1/socialappaut/LoginActivityTest.java
+++ SocialAppAUT/app/src/androidTest/java/com/example/user1/socialappaut/LoginActivityTest.java
@@ -1,9 +1,13 @@
 package com.example.user1.socialappaut;
 
+import android.app.Activity;
 import android.support.test.rule.ActivityTestRule;
+import android.widget.TextView;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Assert.*;
+import
 
 /**
  * Created by Darcy on 25/09/2017.
@@ -16,4 +20,20 @@ public class LoginActivityTest {
 
     public ActivityTestRule<LoginActivity> loginActivityActivityTestRule
             = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    @Test
+    public void testEmptyUsernameTest(){
+        Activity login = loginActivityActivityTestRule.getActivity();
+
+        assertNotNull(login.findViewById(R.id.etStudentID));
+
+        TextView username = (TextView)login.findViewById(R.id.etStudentID);
+
+        if(username.getText().length()==0)
+        {
+
+        }
+
+
+    }
 }
